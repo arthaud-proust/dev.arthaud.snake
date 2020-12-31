9a9cb7a2e7a426cb5f7cbaf8fb292d27228ed12c
Arthaud Proust <arthaudp33@gmail.com>
2020-12-31 18:08:23 +0100

    fix port

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const io = require("socket.io")(server);
-
 const router = express.Router()
 const PORT = process.env.PORT || 8000;
 

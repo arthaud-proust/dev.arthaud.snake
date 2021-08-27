3a30ae72710db512aedc785f005804a6e7c178d7
Arthaud Proust <arthaudp33@gmail.com>
2021-08-27 15:07:54 +0200

    manifest

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const http = require("http");
 const server = http.createServer(app);
 const io = require("socket.io")(server);
 const router = express.Router()
 const PORT = process.env.PORT || 8000;
 const HOST = process.env.HOST || 'localhost';
-
 //add the router folders
 app.use(express.static(__dirname + '/public'));             // Store all assets, js and css files in public folder.
 app.use(express.static(__dirname + '/resources/views'));    // Store all HTML files in view folder.
 
+app.use(cors());
 app.use('/', router);       // add the router
 
 //routes and ioController
